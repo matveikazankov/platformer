dca43dc1d1ade37af1207da51d80a5b74b976f2f
matveikazankov <47327054+matveikazankov@users.noreply.github.com>
2021-06-08 23:43:40 +0300

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,28 @@
+﻿{
+	"version": 1623182639,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/bird-sheet0.png",
+		"images/tiledbackground.png",
+		"images/tiledbackground2.png",
+		"images/background-sheet0.png",
+		"images/bottompipe-sheet0.png",
+		"images/toppipe-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/140280b161b409e9f5bcbdaf2ad440f-sheet0.png",
+		"images/il_570xn_-sheet0.png",
+		"images/il_570xn_2-sheet0.png",
+		"images/140280b161b409e9f5bcbdaf2ad440f4_-sheet0.png",
+		"media/bruh_-_welcome_to_the_cum_zone_(gybka.com).m4a",
+		"media/bruh_-_welcome_to_the_cum_zone_(gybka.com).ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
